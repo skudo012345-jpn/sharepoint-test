42406c120c0749b67324388881ff7a1d9a59a9a0
skudo012345-jpn <skudo.012345@gmail.com>
2026-05-14 14:24:54 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
-console.log("GitHub Pages JS loaded");
+console.log("JS loaded");
+
 window.__test = "OK";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.createElement("button");
+  btn.innerText = "Click me";
+
+  btn.onclick = function () {
+    alert("ボタンが押された");
+  };
+
+  document.body.appendChild(btn);
+});
